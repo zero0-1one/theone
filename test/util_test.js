c1dee15a28a5f890c9d7a9be3c11f83ea72b6861
ygy <zero0_1one@163.com>
2020-02-01 12:22:30 +0800

    lock 替换成更可控的 doOrder

--- test/util_test.js
+++ test/util_test.js
@@ -3,19 +3,21 @@
 const assert = require('chai').assert
 const toUtil = require('../lib/util')
 
-
 describe('util', function() {
   it('lock', async function() {
     let tasks = []
-    tasks.push(toUtil.lock('a', async () => {
-      await toUtil.sleep(1000)
-    }))
-    tasks.push(toUtil.lock('a', async () => {
-      await toUtil.sleep(1000)
-    }))
+    tasks.push(
+      toUtil.lock('a', async () => {
+        await toUtil.sleep(1000)
+      })
+    )
+    tasks.push(
+      toUtil.lock('a', async () => {
+        await toUtil.sleep(1000)
+      })
+    )
     await Promise.all(tasks)
   })
-
 
   it('passwordHash, passwordVerify', function() {
     let hash = toUtil.passwordHash('123456')
