edbfca34b0be4463898117efa4ca6e56b006e934
ygy <zero0_1one@163.com>
2020-03-13 17:40:41 +0800

    config  支持 path obj 配置

--- test/util_test.js
+++ test/util_test.js
@@ -3,8 +3,38 @@
 const assert = require('chai').assert
 const toUtil = require('../lib/util')
 
-describe('util', function() {
-  it('lock', async function() {
+describe('util', function () {
+  it('deepAssign', async function () {
+    let a = {
+      b: {
+        c: [1, 2, { d: 3 }],
+        e: 'e1'
+      },
+      c: 1
+    }
+
+    let b = {
+      'b.c.1': 5,
+      'b.c.2.d': 6,
+      b: {
+        e: 'e2'
+      },
+      f: [1, 2]
+    }
+
+    toUtil.deepAssign(a, b)
+    assert.deepEqual(a, {
+      b: {
+        c: [1, 5, { d: 6 }],
+        e: 'e2'
+      },
+      c: 1,
+      f: [1, 2]
+    })
+  })
+
+
+  it('lock', async function () {
     let tasks = []
     tasks.push(
       toUtil.lock('a', async () => {
@@ -19,7 +49,7 @@ describe('util', function() {
     await Promise.all(tasks)
   })
 
-  it('passwordHash, passwordVerify', function() {
+  it('passwordHash, passwordVerify', function () {
     let hash = toUtil.passwordHash('123456')
     assert.isTrue(toUtil.passwordVerify('123456', hash))
     assert.isFalse(toUtil.passwordVerify('a123456', hash))
