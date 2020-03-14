e1fcbda593bb77e6df56bfefdbb3880f24689190
ygy <zero0_1one@163.com>
2020-03-14 23:50:14 +0800

    废弃rememver 函数, 更名为 remember

--- test/cache_test.js
+++ test/cache_test.js
@@ -11,12 +11,12 @@ const options = {
   'timeout': 1
 }
 
-describe('cache', function() {
-  it('init', async function() {
+describe('cache', function () {
+  it('init', async function () {
     cache.init(options)
   })
 
-  it('set and get', async function() {
+  it('set and get', async function () {
     let a = await cache.get('a')
     assert.isUndefined(a)
     a = await cache.set('a', { a: 11, b: [1, 2, 3] })
@@ -24,7 +24,7 @@ describe('cache', function() {
     assert.deepEqual(await cache.get('a'), { a: 11, b: [1, 2, 3] })
   })
 
-  it('timeout', async function() {
+  it('timeout', async function () {
     await cache.set('a', 'aaaa')
     await cache.set('b', 'bbbb', 2)
     assert.equal(await cache.get('a'), 'aaaa')
@@ -34,7 +34,7 @@ describe('cache', function() {
     assert.equal(await cache.get('b'), 'bbbb')
   })
 
-  it('clear', async function() {
+  it('clear', async function () {
     await cache.set('a', 'aaaa')
     assert.equal(await cache.get('a'), 'aaaa')
     await cache.clear('a')
@@ -42,12 +42,12 @@ describe('cache', function() {
   })
 
   let num
-  its_par(100, 'rememver', async function() {
+  its_par(100, 'remember', async function () {
     await this.beforeAll(async () => {
       num = 0
       await cache.clear('a')
     })
-    let a = await cache.rememver('a', async function() {
+    let a = await cache.remember('a', async function () {
       await toUtil.sleep(10)
       return ++num
     })
@@ -55,7 +55,7 @@ describe('cache', function() {
     assert.equal(await cache.get('a'), a)
   })
 
-  it('cache:set and get', async function() {
+  it('cache:set and get', async function () {
     await cache.clear('a')
     let a = await cache('a')
     assert.isUndefined(a)
@@ -64,12 +64,12 @@ describe('cache', function() {
     assert.deepEqual(await cache('a'), { a: 11, b: [1, 2, 3] })
   })
 
-  its_par(100, 'cache:rememver', async function() {
+  its_par(100, 'cache:remember', async function () {
     await this.beforeAll(async () => {
       num = 0
       await cache.clear('a')
     })
-    let a = await cache('a', async function() {
+    let a = await cache('a', async function () {
       await toUtil.sleep(10)
       return ++num
     })
@@ -77,14 +77,14 @@ describe('cache', function() {
     assert.equal(await cache('a'), a)
   })
 
-  it('cache:clear', async function() {
+  it('cache:clear', async function () {
     await cache('a', 'aaaa')
     assert.equal(await cache('a'), 'aaaa')
     cache('a', null)
     assert.isUndefined(await cache('a'))
   })
 
-  it('close', async function() {
+  it('close', async function () {
     cache.close()
   })
 })
