1b0310b0bd69944c8579d07d8079647fcf533484
ygy358585315@163.com <ygy358585315@163.com>
2020-12-30 10:15:48 +0800

    修改 memory 测试用例

--- test/memoryCache_test.js
+++ test/memoryCache_test.js
@@ -3,7 +3,6 @@
 const MemoryCache = require('../lib/cacheAdapter/memory')
 const { assert } = require('chai')
 
-
 let cache = new MemoryCache()
 
 function isExpired(data) {
@@ -11,55 +10,55 @@ function isExpired(data) {
 }
 
 describe('memoryCache', function () {
-  it('set', function () {
-    cache.set('name', { expired: false, data: 'no tag' })
-    cache.set('name', { expired: false, data: 'tagA' }, 'tagA')
-    cache.set('name', { expired: true, data: 'tagB' }, 'tagB')
+  it('set', async function () {
+    await cache.set('name', { expired: false, data: 'no tag' })
+    await cache.set('name', { expired: false, data: 'tagA' }, 'tagA')
+    await cache.set('name', { expired: true, data: 'tagB' }, 'tagB')
   })
 
-  it('get', function () {
-    let noTag = cache.get('name')
-    let tagA = cache.get('name', 'tagA')
-    let tagB = cache.get('name', 'tagB')
+  it('get', async function () {
+    let noTag = await cache.get('name')
+    let tagA = await cache.get('name', 'tagA')
+    let tagB = await cache.get('name', 'tagB')
     assert.deepEqual(noTag, { expired: false, data: 'no tag' })
     assert.deepEqual(tagA, { expired: false, data: 'tagA' })
     assert.deepEqual(tagB, { expired: true, data: 'tagB' })
   })
 
-  it('clear', function () {
-    cache.clear('name')
-    cache.clear('name', 'tagA')
-    cache.clear('name', 'tagB')
-    let noTag = cache.get('name')
-    let tagA = cache.get('name', 'tagA')
-    let tagB = cache.get('name', 'tagB')
+  it('clear', async function () {
+    await cache.clear('name')
+    await cache.clear('name', 'tagA')
+    await cache.clear('name', 'tagB')
+    let noTag = await cache.get('name')
+    let tagA = await cache.get('name', 'tagA')
+    let tagB = await cache.get('name', 'tagB')
     assert.isUndefined(noTag)
     assert.isUndefined(tagA)
     assert.isUndefined(tagB)
   })
 
-  it('clearTag', function () {
-    cache.set('a1', 'a', 'tagA')
-    cache.set('b1', 'b', 'tagB')
-    cache.set('b2', 'c', 'tagB')
-    cache.set('b3', 'd', 'tagB')
-    cache.clearTag('tagB')
+  it('clearTag', async function () {
+    await cache.set('a1', 'a', 'tagA')
+    await cache.set('b1', 'b', 'tagB')
+    await cache.set('b2', 'c', 'tagB')
+    await cache.set('b3', 'd', 'tagB')
+    await cache.clearTag('tagB')
 
-    assert.equal(cache.get('a1', 'tagA'), 'a')
-    assert.isUndefined(cache.get('b1', 'tagB'), 'b')
-    assert.isUndefined(cache.get('b2', 'tagB'), 'c')
-    assert.isUndefined(cache.get('b3', 'tagB'), 'd')
+    assert.equal(await cache.get('a1', 'tagA'), 'a')
+    assert.isUndefined(await cache.get('b1', 'tagB'), 'b')
+    assert.isUndefined(await cache.get('b2', 'tagB'), 'c')
+    assert.isUndefined(await cache.get('b3', 'tagB'), 'd')
   })
 
-  it('gc', function () {
-    cache.set('name', { expired: false, data: 'no tag' })
-    cache.set('name', { expired: false, data: 'tagA' }, 'tagA')
-    cache.set('name', { expired: true, data: 'tagB' }, 'tagB')
-    cache.gc(isExpired)
+  it('gc', async function () {
+    await cache.set('name', { expired: false, data: 'no tag' })
+    await cache.set('name', { expired: false, data: 'tagA' }, 'tagA')
+    await cache.set('name', { expired: true, data: 'tagB' }, 'tagB')
+    await cache.gc(isExpired)
 
-    let noTag = cache.get('name')
-    let tagA = cache.get('name', 'tagA')
-    let tagB = cache.get('name', 'tagB')
+    let noTag = await cache.get('name')
+    let tagA = await cache.get('name', 'tagA')
+    let tagB = await cache.get('name', 'tagB')
     assert.deepEqual(noTag, { expired: false, data: 'no tag' })
     assert.deepEqual(tagA, { expired: false, data: 'tagA' })
     assert.isUndefined(tagB)
